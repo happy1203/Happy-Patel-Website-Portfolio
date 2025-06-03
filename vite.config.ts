6c3b88966b74afdb15f2b1121d8326000f9b31a3
happy1203 <96698532+happy1203@users.noreply.github.com>
2025-06-03 14:53:48 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Happy-Patel-Website-Portfolio/",  // 👈 this is the critical fix!
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
